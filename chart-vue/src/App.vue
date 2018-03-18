500cdbe40ea2591e305ea1e547ed095c81654744
jieun <jieun4140@gmail.com>
2018-03-19 01:12:28 +0900

    vue 입문, hightchart 추가

---
+++ chart-vue/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <button type="button" class="btn btn-primary btn-lg" v-on:click="toggle">Chart Click!!!</button>
+    <span v-show="isOpen"><Chart></Chart></span>
+  </div>
+</template>
+
+<script>
+import Chart from './Chart.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      isOpen: false
+    }
+  },
+  components: {
+    Chart
+  },
+  methods: {
+    toggle: function(){
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ chart-vue/src/Chart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="container" style="width:100%; height:400%"></div>
+</template>
+<script>
+export default {
+  mounted() {
+    Highcharts.chart('container', {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Fruite Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Orange']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      }, {
+        name: 'Jieun',
+        data: [5, 7, 3]
+      }]
+    })
+  }
+}
+</script>
